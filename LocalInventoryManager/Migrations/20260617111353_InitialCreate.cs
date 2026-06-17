01333a4d24865a82e8124afa86d0f3e9352d5797
Ephraim <2lanieph@gmail.com>
2026-06-17 13:16:39 +0200

    Just migrated product database

---
+++ LocalInventoryManager/Migrations/20260617111353_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalInventoryManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Sku = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ProductName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    LowStockThreshold = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ LocalInventoryManager/Models/InventoryDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace LocalInventoryManager.Models
+{
+    internal class InventoryDbContext : DbContext
+    {
+        //Maps the InventoryItem class to the database table
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Get the path to the user's local application data folder
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //Combine the local application data folder path with the name of the database file
+            string folderPath = Path.Combine(appDataPath, "LocalInventoryManager");
+
+            //Create the directory if it doesn't exist
+            Directory.CreateDirectory(folderPath);
+            string dbPath = Path.Combine(folderPath, "LocalInventory.db");
+
+            //Configure the DbContext to use SQLite with the specified database file path
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
