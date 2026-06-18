4570cf58d2abbba814ae869f79892eb4ac839df6
Ephraim <2lanieph@gmail.com>
2026-06-18 14:17:34 +0200

    Created the first dashboard screen

--- LocalInventoryManager/Views/MainWindow.xaml.cs
+++ LocalInventoryManager/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LocalInventoryManager
+namespace LocalInventoryManager.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
