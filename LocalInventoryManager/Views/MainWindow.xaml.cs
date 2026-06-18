8bf14f3c7b58a438b4c6eb481058e76d30d13424
Ephraim <2lanieph@gmail.com>
2026-06-18 14:47:42 +0200

    fixed issues

--- LocalInventoryManager/Views/MainWindow.xaml.cs
+++ LocalInventoryManager/Views/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace LocalInventoryManager.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(
+                new Action(CenterWindowOnScreen),
+                System.Windows.Threading.DispatcherPriority.ApplicationIdle
+            );
+        }
+
+        private void CenterWindowOnScreen()
+        {
+            var area = Screen.PrimaryScreen.WorkingArea;
+
+            Left = area.Left + (area.Width - ActualWidth) / 2;
+            Top = area.Top + (area.Height - ActualHeight) / 2;
         }
     }
 }
